d3b0d9d5f1fc5e0997a4ba0db17a4d11acb201a5
Josef Zoller <josef@walterzollerpiano.com>
2024-10-29 14:14:27 +0100

    Screenshot all displays on lock

--- hypr/scripts/lock.sh
+++ hypr/scripts/lock.sh
@@ -2,6 +2,25 @@
 
 pidof hyprlock && exit
 
-grim -o eDP-1 ~/tmp/lock.png
-hyprlock
+monitors_config=""
+for monitor in $(hyprctl monitors -j | jq -r '.[].name'); do
+    grim -o $monitor ~/tmp/lock_$monitor.png
+
+    monitors_config+="background {\\n\
+    monitor = $monitor\\n\
+    path = ~/tmp/lock_$monitor.png\\n\
+    color = rgb(54, 12, 106)\\n\
+\\n\
+    blur_passes = 5\\n\
+    blur_size = 1\\n\
+    brightness = 0.4\\n\
+}\\n\
+\\n"
+done
+
+cp ~/.config/hypr/hyprlock.conf ~/tmp/hyprlock.conf
+
+sed -z "s|# monitors\\n\\n|$monitors_config|g" -i ~/tmp/hyprlock.conf
+
+hyprlock -c ~/tmp/hyprlock.conf
 
